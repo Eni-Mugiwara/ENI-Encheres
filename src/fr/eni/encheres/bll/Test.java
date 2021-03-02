cee6b778188841b698f32a71fb89b530387205bc
Safa-Le-Faire-Tinquiete <75102678+Safa-Le-Faire-Tinquiete@users.noreply.github.com>
2021-03-02 16:04:04 +0100

    "hey"

--- src/fr/eni/encheres/bll/Test.java
+++ src/fr/eni/encheres/bll/Test.java
@@ -3,7 +3,7 @@ package fr.eni.encheres.bll;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hey, je suis une tricheuse");
+		System.out.println("Hey");
 
 	}
 
